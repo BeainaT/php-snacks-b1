b31a9e6bcdacd8773c8d5b0bda15b6d80d68ccc7
BeainaT <tania.df@hotmail.it>
2022-06-22 15:42:52 +0200

    added snack-1 only php

--- snack-1/index.php
+++ snack-1/index.php
@@ -1,2 +1,35 @@
-<!-- Creiamo un array contenente le partite di basket di un’ipotetica tappa del calendario. Ogni array avrà una squadra di casa e una squadra ospite, punti fatti dalla squadra di casa e punti fatti dalla squadra ospite. Stampiamo a schermo tutte le partite con questo schema.
-Olimpia Milano - Cantù | 55-60 -->
+<?php
+//Set an array that contains basket matches
+$basketball_matches = [
+    //$basketball_matches contains match info (Home Team, Away Team and their respective score) as associative array
+    [
+        'HomeTeam' => 'Olimpia Milano',
+        'AwayTeam' => 'Virtus Bologna',
+        'HomeScores' => 77,
+        'AwayScores' => 62,
+    ],
+    [
+        'HomeTeam' => 'Venezia',
+        'AwayTeam' => 'Gran Canaria',
+        'HomeScores' => 72,
+        'AwayScores' => 83,
+    ],
+    [
+        'HomeTeam' => 'Trento',
+        'AwayTeam' => 'VL Pesaro',
+        'HomeScores' => 88,
+        'AwayScores' => 74,
+    ],
+    [
+        'HomeTeam' => 'Valencia',
+        'AwayTeam' => 'Brescia',
+        'HomeScores' => 79,
+        'AwayScores' => 53,
+    ],
+];
+
+// Print all matches in format: Home Team - Away Team | Home Scores - Away Scores
+for ($i = 0; $i < count($basketball_matches); $i++) {
+    echo $basketball_matches[$i]["HomeTeam"] . " - " . $basketball_matches[$i]["AwayTeam"] . " | " . $basketball_matches[$i]["HomeScores"] . " - " . $basketball_matches[$i]["AwayScores"] . "<br>";
+}
+?>
