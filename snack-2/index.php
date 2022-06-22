9b0324d9e5410465a7c9cedab1cfab09c011803e
BeainaT <tania.df@hotmail.it>
2022-06-22 16:52:16 +0200

    added whitespaces name validation+html form

--- snack-2/index.php
+++ snack-2/index.php
@@ -1,15 +1,34 @@
 <?php
 //Pass name, age and mail as GET parameters
-$user_name = $_GET['name'];
+$user_name = trim($_GET['name']);
 $user_age = $_GET['age'];
 $user_mail = $_GET['mail'];
-
-//IF name length > 3 (and isn't a number), age is a number and mail is a valid adress mail 
-if (strlen($user_name) > 3 && !is_numeric($user_name) && is_numeric($user_age) && filter_var($user_mail, FILTER_VALIDATE_EMAIL)) {
-    //print "Accesso Riuscito"
-    echo "Accesso Riuscito";
-//ELSE print "Accesso Negato"
-} else {
-    echo "Accesso Negato";
-}
 ?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Snack-2</title>
+</head>
+<body>
+    <form action="">
+        <input type="text" name="name" placeholder="Enter your name">
+        <input type="text" name="age" placeholder="Enter your age">
+        <input type="text" name="mail" placeholder="Enter your mail">
+        <button type="submit">SEND</button>
+    </form>
+    <?php 
+    //IF name length > 3 (and isn't a number), age is a number and mail is a valid adress mail 
+    if (strlen($user_name) > 3 && !is_numeric($user_name) && is_numeric($user_age) && filter_var($user_mail, FILTER_VALIDATE_EMAIL)) {
+        //print "Accesso Riuscito"
+        echo "Accesso Riuscito";
+    //ELSE print "Accesso Negato"
+    } else {
+        echo "Accesso Negato";
+    }
+    ?>
+
+</body>
+</html>
